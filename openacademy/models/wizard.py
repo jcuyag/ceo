993b3c340ea63aef996d8cf5a95a6b0550caa182
Jesse Cuyag <jesse.cuyag@cloudemployee.co.uk>
2017-01-20 21:24:58 +1100

    wizard

--- openacademy/models/wizard.py
+++ openacademy/models/wizard.py
@@ -2,15 +2,20 @@
 
 from odoo import models, fields, api
 
-class Wizard(models.Model):
+class Wizard(models.TransientModel):
     _name = 'openacademy.wizard'
 
-    def _default_session(self):
-        return self.env['openacademy.session'].browse(self._context.get('active_id'))
-
-    session_id = fields.Many2one(string='Session',
-                                 required=True,
-                                 default=_default_session,
-                                 comodel_name='openacademy.session')
+    def _default_sessions(self):
+        return self.env['openacademy.session'].browse(self._context.get('active_ids'))
+        
     attendee_ids = fields.Many2many(string='Attendees',
                                     comodel_name='res.partner')
+
+    session_ids = fields.Many2many('openacademy.session',
+        string="Sessions", required=True, default=_default_sessions)
+
+    @api.multi
+    def subscribe(self):
+        for session in self.session_ids:
+            session.attendee_ids |= self.attendee_ids
+        return {}
